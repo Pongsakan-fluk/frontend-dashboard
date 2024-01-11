690212780abb4d16dbf47182c02a5b2b89e8238f
Pongsakan-fluk <pongsakan.obr@futurelifestyle.co.th>
2024-01-11 16:55:06 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+import TrafficSource from "./components/contents/ui-charts/Traffic-source";
+
+function App() {
+  const [data, setData] = useState([
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ]);
+
+  return (
+    <>
+      <h1 className="text-3xl text-blue-500 font-bold underline">
+        Hello world!
+      </h1>
+      <button className="btn btn-outline btn-success">Primary</button>
+
+      <TrafficSource />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/contents/ui-charts/Traffic-source.jsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+function TrafficSource() {
+  const [data, setData] = useState([
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ]);
+  return (
+    <div className="w-full bg-slate-200 flex flex-col justify-center px-10">
+      <div className="my-2 flex justify-between items-center">
+        <h3>Traffic sources</h3>
+        <button className="btn btn-warning">Action</button>
+      </div>
+      <hr className="bg-blue-500" />
+
+      <div className="mx-auto my-5">
+        <BarChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+          barSize={20}
+        >
+          <XAxis
+            dataKey="name"
+            scale="point"
+            padding={{ left: 10, right: 10 }}
+          />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Bar dataKey="pv" fill="#8884d8" background={{ fill: "#eee" }} />
+        </BarChart>
+      </div>
+    </div>
+  );
+}
+
+export default TrafficSource;
